ebf0f4a8994e1a50642736adc91bdac0ca29d430
Alex James <theracermaster@gmail.com>
2016-12-23 13:51:25 -0600

    Clover: Add gen-version.sh to generate Version.h
    Version.h defines the revision and other build information for Clover.
    Create gen-version.sh to automatically generate Version.h and fix the
    git-svn history if it is broken.

---
+++ gen-version.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Generate Version.h for Clover
+
+# Usage: ./gen-version.sh <build info string>
+#
+# <build info string>    Optional string (used to define compiler/build options)
+#                        This isn't required; Version.h will be created without it
+#                        if a <build info string> isn't specified
+#
+
+# Fix git-svn history if it doesn't exist
+if [ -z "$(git show-ref | grep refs/remotes/git-svn)" ]; then
+	git svn init svn://svn.code.sf.net/p/cloverefiboot/code/
+	git update-ref refs/remotes/git-svn $(git rev-parse HEAD)
+	git svn rebase
+fi
+
+SVN_REVISION=$(git svn info | grep Revision | awk '{print $2}')
+SUFFIX="RM-$(git rev-parse --short HEAD)"
+REVISION="$SVN_REVISION-$SUFFIX"
+BUILD_DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
+if [ -e Version.h ]; then
+	rm -f Version.h
+fi
+
+echo -e \#define FIRMWARE_VERSION \"2.31\"\ > Version.h
+echo -e \#define FIRMWARE_BUILDDATE \"$BUILD_DATE\"\ >> Version.h
+echo -e \#define FIRMWARE_REVISION L\"$REVISION\"\ >> Version.h
+echo -e \#define REVISION_STR \"Clover revision: $REVISION\" >> Version.h
+
+if [ ! -z "$1" ]; then
+	echo -e \#define BUILDINFOS_STR \"$1\" >> Version.h
+fi
