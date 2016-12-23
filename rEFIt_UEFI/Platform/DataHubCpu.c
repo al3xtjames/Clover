a9e2f6747b4bc4f084c5736bdef7a3ab6e361700
Alex James <theracermaster@gmail.com>
2016-12-23 15:15:19 -0600

    Platform/DataHubCpu: Do not delete the `nvda_drv` NVRAM variable
    a1914f1 added support for setting the `nvda_drv` NVRAM variable due to
    changes in the Sierra Web Drivers (`nvda_drv` is no longer a boot arg).
    However, it also deleted the variable if the NvidiaWeb option was not
    set in config.plist, removing the choice selected in the NVIDIA Driver
    Manager. Do not delete the `nvda_drv` NVRAM variable if NvidiaWeb is
    not set in config.plist.

--- rEFIt_UEFI/Platform/DataHubCpu.c
+++ rEFIt_UEFI/Platform/DataHubCpu.c
@@ -320,8 +320,6 @@ SetVariablesForOSX(LOADER_ENTRY *Entry)
   if (gSettings.NvidiaWeb) {
     NvidiaWebValue = "1";
     SetNvramVariable(L"nvda_drv", &gEfiAppleBootGuid, Attributes, 2, (VOID*)NvidiaWebValue);
-  } else {
-    DeleteNvramVariable(L"nvda_drv", &gEfiAppleBootGuid);
   }
   
   if (!gDriversFlags.AptioMemFixLoaded) {
