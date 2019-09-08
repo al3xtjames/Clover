4c198a03df8d8792ac7f044c6eef0a727f19b89c
asava <asava@52d7a1c0-eba5-4e63-a9d7-e3186cd7c462>
2019-09-08 08:47:18 +0000

    Exclude sys.setdefaultencoding for python3, as it is no longer supported, to fix errors on Catalina/XCode10
    git-svn-id: svn://svn.code.sf.net/p/cloverefiboot/code@5078 52d7a1c0-eba5-4e63-a9d7-e3186cd7c462

---
+++ Patches_for_EDK2/BaseTools/Source/Python/sitecustomize.py
@@ -0,0 +1,16 @@
+## @file
+#
+#
+# Copyright (c) 2009 - 2014, Apple Inc. All rights reserved.<BR>
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+import sys
+import locale
+
+if sys.platform == "darwin":
+  DefaultLocal = locale.getdefaultlocale()[1]
+  if DefaultLocal is None:
+    DefaultLocal = 'UTF8'
+if sys.version_info[0] < 3:
+  sys.setdefaultencoding(DefaultLocal)
+
